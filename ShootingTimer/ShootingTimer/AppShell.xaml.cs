61cd14ad0ac865c54d0f607b814292c2755e26aa
asWorks <x10@asWorks.de>
2020-05-04 21:00:17 +0200

    Add project files.

---
+++ ShootingTimer/ShootingTimer/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShootingTimer
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShootingTimer/ShootingTimer/ViewModels/TimerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShootingTimer.ViewModels
+{
+   public class TimerViewModel:BaseViewModel
+    {
+
+
+
+        public TimerViewModel()
+        {
+            Title = "Timerpage";
+            Ooops = "Und auch das funktioniert";
+        }
+
+
+
+        private string _Ooops;
+        public string Ooops
+        {
+            get { return _Ooops; }
+            set { SetProperty(ref this._Ooops, value); }
+
+        }
+
+
+       
+    }
+}
